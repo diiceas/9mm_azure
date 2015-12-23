b8e4e8ea72915839c62809fc42150f980668a621
diiceas <forthepost@gmail.com>
2015-12-23 13:00:36 +0100

    mvc 4 to mvc 6

---
+++ src/azure_9mm/Controllers/DefaultController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Logging;
+using azure_9mm.Models;
+using azure_9mm.Services;
+using azure_9mm.ViewModels.Account;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace razor_9mm.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly IApplicationEnvironment _appEnvironment;
+
+        public DefaultController(IApplicationEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.pageName = "Main";
+            return View();
+        }
+
+        public ActionResult Audio()
+        {
+            ViewBag.pageName = "Audio";
+            return View();
+        }
+
+        public ActionResult Video()
+        {
+            ViewBag.pageName = "Video";
+            return View();
+        }
+
+        public ActionResult Info()
+        {   
+            ViewBag.pageName = "Info";
+            return View();
+        }
+
+        public ActionResult News(int? skin = null)
+        {
+            ViewBag.pageName = "News";
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            string basepath = _appEnvironment.ApplicationBasePath;
+            string[] files = System.IO.Directory.EnumerateFiles(basepath + @"\wwwroot\images\9mm_orig\").Select(path => new FileInfo(path).Name).ToArray();
+            ViewBag.files = files;
+            ViewBag.pageName = "Gallery";
+            return View();
+        }
+    }
+}
